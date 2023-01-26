7cb25bc92b93bd26ccaff9378b13fd0501d59d9e
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-26 21:52:59 +0400

    robo

---
+++ peering.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  peer_owner_id = "827190588206"
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.default_vpc_id
+  peer_region   = "us-east-1"
+  default_vpc_cidr_block  = var.default_vpc_cidr_block
+  default_route_table = var.default_route_table
+}
