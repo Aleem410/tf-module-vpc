8a5ff5772977f6450fe5cfdf54c8c8b916d88b40
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-30 21:30:13 +0400

    robo

--- gateways.tf
+++ gateways.tf
@@ -9,7 +9,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_eip" "ngw" {}
 
 resource "aws_nat_gateway" "natgw" {
-  allocation_id = aws_eip.ngw
+  allocation_id = aws_eip.ngw.id
   subnet_id     = aws_subnet.public_subnets.*.id[0]
 
   tags = {
