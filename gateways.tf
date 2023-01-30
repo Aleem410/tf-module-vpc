56b540b3ff3fb35dcc4aad0fc4e8aa6ac00a82d3
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-30 21:28:12 +0400

    robo

---
+++ gateways.tf
@@ -0,0 +1,17 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}_igw"
+  }
+}
+
+resource "aws_eip" "ngw" {}
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.ngw
+  subnet_id     = aws_subnet.public_subnets.*.id[0]
+
+  tags = {
+    Name = "${var.env}_natgw"
+  }
+}
