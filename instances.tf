a2fee11c819918441abf8e3c5f633663fc43f416
Abdul Aleem <ma.aleem20@gmail.com>
2023-02-01 18:55:28 +0400

    robo

--- instances.tf
+++ instances.tf
@@ -1,18 +1,21 @@
 resource "aws_instance" "public" {
   ami  =  "ami-0a017d8ceb274537d"
-  vpc_security_group_ids = aws_security_group.allow_tls.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   subnet_id = aws_subnet.public_subnets.*.id[0]
 }
 
 resource "aws_instance" "app" {
   ami  =  "ami-0a017d8ceb274537d"
-  vpc_security_group_ids = aws_security_group.allow_tls.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   subnet_id = aws_subnet.app_subnets.*.id[0]
 }
 
 resource "aws_instance" "db" {
   ami  =  "ami-0a017d8ceb274537d"
-  vpc_security_group_ids = aws_security_group.allow_tls.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   subnet_id = aws_subnet.db_subnets.*.id[0]
 }
 
